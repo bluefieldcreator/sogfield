de2d12962d0bcb1c5f3787a3e17c18400a405eed
Bluefield <notshigetora@gmail.com>
2024-08-25 14:22:48 +0200

    fix: change classes

--- addons/buckshot_enhanced/cfgWeapons.hpp
+++ addons/buckshot_enhanced/cfgWeapons.hpp
@@ -1,9 +1,9 @@
-class cfgWeapons {
-  class vn_rifle;
-  class vn_m79: vn_rifle {
-    class vn_m79_muzzle;
-    class vn_m79_shot_muzzle: vn_m79_muzzle {
+class CfgWeapons {
+    class Rifle_Base_F;
+    class vn_rifle: Rifle_Base_F {
+        class vn_m79_muzzle;
+        class vn_m79_shot_muzzle: vn_m79_muzzle {
         magazines[] += { "sogfield_40mm_576_buck_mag" };
+        };
     };
-  };
 };
